f41ac1ce9a2d04117d23c02c7039c04a8b9d55f1
Eric <8BitRain@gmail.com>
2015-11-04 10:04:47 -0600

    Added Assets and Project Settings

---
+++ Assets/Scripts/SimpleCrosshair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCrosshair : MonoBehaviour {
+
+	public Camera cameraFacing;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (cameraFacing.transform.position);
+		transform.Rotate (0.0f, 180.0f, 0.0f);
+		transform.position = cameraFacing.transform.position + cameraFacing.transform.rotation * Vector3.forward;
+	}
+}
