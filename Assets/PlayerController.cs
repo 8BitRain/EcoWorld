f172f4abd0745c2abd34631f324fd4d0705acd01
Eric <8BitRain@gmail.com>
2015-11-15 23:24:37 -0600

    Working on InteractiveObjScript & Player Script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public enum PlayerState{HOLDING, NEUTRAL};
+	public PlayerState playerState;
+
+	// Use this for initialization
+	void Start () {
+		this.playerState = PlayerState.NEUTRAL;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/InteractiveObj.cs
+++ Assets/Scripts/InteractiveObj.cs
@@ -17,7 +17,8 @@ public class InteractiveObj : MonoBehaviour {
 
 	public GameObject player;
 	public bool playerCanInteract;
-
+	public PlayerController playerController;
+	
 	public bool objectHit;
 	Renderer rend;
 	Color initRendColor;
@@ -25,6 +26,7 @@ public class InteractiveObj : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
+		playerController = player.GetComponent<PlayerController> ();
 		objectHit = false;
 		rend = this.GetComponent<Renderer>();
 		initRendColor = rend.sharedMaterial.color;
@@ -40,7 +42,7 @@ public class InteractiveObj : MonoBehaviour {
 		float distance = Vector3.Distance (this.transform.position, player.transform.position);
 		print (distance);
 		//Is the Player within a distance where she can interact with the object?
-		if (objectHit) {
+		if (objectHit && playerController.playerState == PlayerController.PlayerState.NEUTRAL) {
 			if (distance <= 3) {
 				playerCanInteract = true;
 				//print ("Player Can Interact");
@@ -51,23 +53,34 @@ public class InteractiveObj : MonoBehaviour {
 			playerCanInteract = false;
 		}
 
-		//If the Player can interact can she Equip the Item/Press the Item/
+		//If the Player can interact can she Equip the Item/Press the Item/ //Needs to be decided by
+		//each individual object
 		if (playerCanInteract) {
 			if(objType == InteractiveObjType.EQUIPPABLE){
 				if(Input.GetKey(KeyCode.Space)){
 					this.transform.parent = GameObject.Find("Player").transform;
 					print ("This occured");
 					this.playerCanInteract =false;
-					playerIsHolding = true;
+					//playerController.
+					playerController.playerState = PlayerController.PlayerState.HOLDING;
+					//playerIsHolding = true;
 				}
 			}
 		}
-		//Highlight Object being viewed by reticle
-		if (objectHit && !playerIsHolding) {
-			rend.sharedMaterial.color = Color.yellow;
-		} else {
-			rend.sharedMaterial.color = initRendColor;
+		//Highlight Object being viewed by reticle 
+		//Based on if the playerIsHolding something. If she is change this value
+		if (!playerIsHolding) {
+			if (objectHit && !playerIsHolding) {
+				rend.sharedMaterial.color = Color.yellow;
+			}
+			if(!objectHit && !playerIsHolding){
+				rend.sharedMaterial.color = initRendColor;
+			}
+			if (playerIsHolding) {
+				rend.sharedMaterial.color = initRendColor;
+			}
 		}
+	
 
 	}
 
