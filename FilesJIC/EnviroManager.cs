9d75cb8a0e247169e9b33c2e3ded3469a6560cca
Eric <8BitRain@gmail.com>
2015-12-04 14:57:27 -0600

    Adding these files for khang

---
+++ FilesJIC/EnviroManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class EnviroManager : MonoBehaviour {
+
+	public GameObject enviroHazardMeter;
+	RectTransform enviroHazardMeterTransform;
+	Slider enviroHazardSlider;
+	GameObject[] containerObjs;
+	GameObject[] switchObjs;
+
+	//WeatherEffects
+	//MagmaRain
+	public GameObject magmaRain;
+	public Transform magmaRainSpawn;
+	private bool magmaRainSpawned;
+
+	//FireMist
+	public GameObject fireMist;
+	public Transform fireMistSpawn;
+	private bool fireMistSpawned;
+
+	private int numObjsInWater;
+	private int prevNumObjsInWater;
+	// Use this for initialization
+	void Start () {
+		enviroHazardMeterTransform = enviroHazardMeter.GetComponent<RectTransform> (); 
+		enviroHazardSlider = enviroHazardMeter.GetComponent<Slider> ();
+		//enviroHazardMeterTransform.transform.
+		enviroHazardMeterTransform.sizeDelta = new Vector2 (Screen.width, 40);
+		containerObjs = GameObject.FindGameObjectsWithTag ("CONTAINER"); 
+		switchObjs = GameObject.FindGameObjectsWithTag("SWITCH");
+
+		numObjsInWater = 0;
+		prevNumObjsInWater = 0; 
+
+		//Weather Initialization
+		magmaRainSpawned = false;
+		fireMistSpawned = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//CountWaterObjs and check to see if water needs to be incremented
+		countWaterObjs ();
+		if (prevNumObjsInWater < numObjsInWater) {
+			print (prevNumObjsInWater);
+			increaseEnviromentMeter((numObjsInWater - prevNumObjsInWater)*15);
+		}
+
+	}
+
+	int countWaterObjs(){
+		//GameObject.FindGameObjectsWithTag ("CONTAINER").Length;
+		prevNumObjsInWater = numObjsInWater;
+		numObjsInWater = 0;
+		for (int i = 0; i < containerObjs.Length; i++) {
+			numObjsInWater += containerObjs[i].GetComponent<InteractiveObj>().numObjs;
+			checkWeatherEffect();
+		}
+		return numObjsInWater;
+	}
+
+	void increaseEnviromentMeter(int incrementAmnt){
+		//Need a value for increment maybe by 5?
+		enviroHazardSlider.value += incrementAmnt;
+	}
+
+	void checkWeatherEffect(){
+		if (enviroHazardSlider.value > 50 && !magmaRainSpawned) {
+			Instantiate(magmaRain, magmaRainSpawn.position, magmaRainSpawn.rotation);
+			magmaRainSpawned = true;
+		}
+
+		if (enviroHazardSlider.value >= 75 && !fireMistSpawned) {
+			Instantiate(fireMist, fireMistSpawn.position, fireMistSpawn.rotation);
+			fireMistSpawned = true;
+		}
+	}
+
+	void getContainers(){
+
+	}
+}
---
+++ FilesJIC/SimpleCrosshair.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SimpleCrosshair : MonoBehaviour {
+
+	public Camera CameraFacing;
+	public float[] values;
+	private Vector3 originalScale;
+	private InteractiveObj interactiveObj;
+	public GameObject objectViewing;
+	
+	// Use this for initialization
+	void Start () {
+		originalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		float distance;
+		if (Physics.Raycast (new Ray (CameraFacing.transform.position,
+		                              CameraFacing.transform.rotation * Vector3.forward),
+		                     out hit)) {
+			distance = hit.distance;
+			if (hit.transform.GetComponent<InteractiveObj> () != null && hit.transform.tag != "Background") {
+				interactiveObj = hit.transform.GetComponent<InteractiveObj> ();
+				interactiveObj.objectHit = true;
+				objectViewing = hit.transform.gameObject;
+				//print ("Hitting Something");
+			} else{
+				if(interactiveObj != null){
+					//print ("Hitting Nothing");
+					interactiveObj.objectHit = false;
+					objectViewing = null;
+				}
+
+			}
+		} else {
+			distance = CameraFacing.farClipPlane * 0.95f;
+		}
+		transform.position = CameraFacing.transform.position +
+			CameraFacing.transform.rotation * Vector3.forward;
+		transform.LookAt (CameraFacing.transform.position);
+		transform.Rotate (0.0f, 180.0f, 0.0f);
+		if (distance < 10.0f) {
+			distance *= 1 + 5*Mathf.Exp (-distance);
+		}
+		//transform.localScale = originalScale * distance;
+	}
+}
+
+
